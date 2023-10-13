56eb0246c44617583f72e15910e98645a5df03dd
unknown <ngcebenhleshabangu@gmail.com>
2023-10-13 16:58:42 +0200

    added auth and auth-ui

---
+++ init-firebase.js
@@ -0,0 +1,26 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7GVZq_ezMSo_-kpHgT0MYz0PpOikGxD8",
+  authDomain: "ns-instagram-clone.firebaseapp.com",
+  projectId: "ns-instagram-clone",
+  storageBucket: "ns-instagram-clone.appspot.com",
+  messagingSenderId: "407824717845",
+  appId: "1:407824717845:web:d2275c005a1eac8b53561c",
+  measurementId: "G-Z2F3T8RKF9"
+};
+firebase.initializeApp(firebaseConfig);
+
+ // Initialize the FirebaseUI Widget using Firebase.
+ var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  const auth = new firebase.auth();
+ 
+ ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // Other config options...
+});
+
+ 
